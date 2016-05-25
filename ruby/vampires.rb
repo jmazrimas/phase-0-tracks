92de232b7dc64a21186f1707505a67fd7da7bbf0
jmazrimas <joe.mazrimas@gmail.com>
2016-05-25 17:35:13 -0500

    add logic for age checking

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,27 @@
 
 
-puts "What is your name?"
-name=gets.chomp
+# puts "What is your name?"
+# name=gets.chomp
 
 puts "How old are you?"
 age=gets.chomp
+age=age.to_i
 
 puts "What year were you born?"
 year=gets.chomp
+year=year.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-bread=gets.chomp
+# puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+# bread=gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
-health=gets.chomp
+# puts "Would you like to enroll in the company’s health insurance?"
+# health=gets.chomp
+
+currentyear=Time.new.year.to_i
+
+yearage=currentyear-year
+
+if yearage!=age
+	agecheck=false
+else agecheck=true
+end
