fd065d03b40afe9380bd2d68b7de64e6fb990034
jmazrimas <joe.mazrimas@gmail.com>
2016-05-25 11:51:22 -0500

    add logic for updating variable datatypes and outputting back to user

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,9 @@
-
-
 puts "Hamster's name?"
 name=gets.chomp
 
 puts "Hamster's volume level? (1-10)"
 volume=gets.chomp
+volume=volume.to_i
 
 puts "Hamster's fur color?"
 color=gets.chomp
@@ -14,7 +13,11 @@ adoption=gets.chomp
 
 puts "Estimated age?"
 age=gets.chomp
-
 if age==""
-	age=nil
+   age=nil
 end
+if age!=nil
+    age=age.to_i
+end
+
+puts "Input Summary", "Name: #{name}" , "Volume: #{volume}", "Color: #{color}", "Good for adoption? #{adoption}", "Age: #{age}"
