cd94a1e08b0be361c12a10ce2cb1044a60255b8a
jmazrimas <joe.mazrimas@gmail.com>
2016-05-25 17:17:49 -0500

    add basic questions to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+
+
+puts "What is your name?"
+name=gets.chomp
+
+puts "How old are you?"
+age=gets.chomp
+
+puts "What year were you born?"
+year=gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread=gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health=gets.chomp
