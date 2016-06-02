96249c09178b987887e7b674832f61f96ff194ac
jmazrimas <joe.mazrimas@gmail.com>
2016-06-02 10:23:10 -0500

    create methods in alias_manager.rb to advance letters and recompile to new name. Also create user interface to input, store, and read back names.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -22,18 +22,61 @@ def reverse_name(name)
 	name.split(' ').reverse.join(' ')
 end
 
-def advance_letters(name)
+def letter_changer(letter)
 	vowels='aeiou'
 	consonants='bcdfghjklmnpqrstvwxyz'
+	#below is to check for capitalization
+	if ('A'..'Z').include?(letter)
+		vowels=vowels.upcase
+		consonants=consonants.upcase
+	end
+	if vowels.include?(letter)
+		if vowels.index(letter)+1 >= vowels.length
+			vowels[0]
+		else
+			vowels[vowels.index(letter)+1]
+		end
+	elsif consonants.include?(letter)
+		if consonants.index(letter)+1 >= consonants.length
+			consonants[0]
+		else
+			consonants[consonants.index(letter)+1]
+		end
+	else letter
+	end
+end
+
+def advance_letters(name)
 	i=0
 	while i<name.length
-		if name[i].upcase==name[i]
-			
-		else
+		name[i]=letter_changer(name[i])
+		i+=1
 	end
+	name
 end
 
 def alias_maker(name)
 	newname=reverse_name(name)
-
+	advance_letters(newname)
 end
+
+$spy_name_hash={}
+
+def outputspynames
+	$spy_name_hash.each {|realname,aliasname| puts "#{aliasname} is actually #{realname}."}
+end
+
+############# USER INTERFACE
+
+inputname=""
+
+while inputname!="quit"
+	puts "Please provide name to be aliased. To quit, type 'quit' (all lower case)"
+	inputname=gets.chomp
+	if inputname!="quit"
+		$spy_name_hash.store(inputname, alias_maker(inputname))
+		p alias_maker(inputname)
+	end
+end
+
+outputspynames
