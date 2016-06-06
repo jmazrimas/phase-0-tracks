cc54e9e8a1dedb31395091646735a095c551fd87
jmazrimas <joe.mazrimas@gmail.com>
2016-06-06 10:37:34 -0500

    outline notes puppy characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Canine
+
+# CHARACTERISTICS ----------------
+# Breed: varies
+# Wet nose: yes
+# Eye count: 2
+# Has Teeth: yes
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Chase Ball
+# Run
+# Lick Face
+# Destroy Household Object
