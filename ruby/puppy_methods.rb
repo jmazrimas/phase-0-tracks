004fcf92c626f8cf3065408bbe3e15e7ec24fdfe
jmazrimas <joe.mazrimas@gmail.com>
2016-06-07 16:25:25 -0500

    add bikers to hash to use numbers to name them

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -57,8 +57,8 @@
 
 class Biker
   
-  def ride_to(place)
-    puts "I rode to #{place}"
+  def ride_to(biker_name, place)
+    puts "#{biker_name} rode to #{place}"
   end
   
   def change_tire
@@ -73,29 +73,36 @@ class Biker
 end
 
 biker_array = []
+biker_hash = {}
 
 
 # mike = Biker.new
 # biker_array << mike
 
 
-# counter = 1
+counter = 1
 50.times do 
   new_biker = Biker.new
-  biker_array << new_biker
+  #biker_array << new_biker
+  biker_hash[new_biker]="Biker #{counter}"
 #   # bikername= 
 #   "biker#{counter}" = Biker.new
 #   # puts bikername
 #   mike = Biker.new
 #   puts mike
 #   # puts bikername
-  # counter+=1
+  counter+=1
 end
 
+# p biker_hash
 
-biker_array.each do |current_biker|
-  puts current_biker.ride_to("work")
-  puts current_biker.change_tire
+# biker_array.each do |current_biker|
+#   puts current_biker.ride_to("work")
+#   puts current_biker.change_tire
+# end
+
+
+biker_hash.each do |biker_key, biker_name|
+  puts biker_key.ride_to(biker_name, "work")
+  puts biker_key.change_tire
 end
-
-
