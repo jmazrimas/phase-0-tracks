57b026a0dd42beeee3126029f9b57d1433da8e24
jmazrimas <joe.mazrimas@gmail.com>
2016-06-08 09:43:39 -0500

    add getter and setter methods for santa code  and driver code to test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,28 @@ class Santa
 	def eat_milk_and_cookies (cookietype)
 		puts "That was a good #{cookietype}!"
 	end
+
+	def celebrate_birthday
+		@age=+1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking << reindeer
+	end
+	#Setter methods
+	def gender=(gender)
+		@gender=gender
+	end
+	#Getter methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
 end
 
 
@@ -34,4 +56,9 @@ ethnicities.length.times do |i|
 	santas << Santa.new(genders[i % genders.length],ethnicities[i])
 end
 
-p santas
+puts santas[4].age
+puts santas[4].ethnicity
+puts santas[4].celebrate_birthday
+puts santas[4].age
+
+puts santas[4].get_mad_at("Cupid")
