fff555e0d910f97ed3063fb8210e2adb217e5d5b
jmazrimas <joe.mazrimas@gmail.com>
2016-06-08 10:10:14 -0500

    add pseudo for DBC student class

---
+++ ruby/solo.rb
@@ -0,0 +1,11 @@
+
+
+# Creating a class: DBC Student
+# Attributes: age, location, current_week, feeling_good
+# Methods:
+# 	Advance a week
+# 	Change locations
+# 	Get really pissed at [concept]
+# 	Figure out [concept]
+# 	
+
