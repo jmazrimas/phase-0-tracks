624b20584eb660e131addb4ab088976cc788efad
jmazrimas <joe.mazrimas@gmail.com>
2016-06-08 08:26:30 -0500

    add loop to create santa instances from attribute arrays

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,8 +2,12 @@
 
 
 class Santa
-	def initialize
+	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -16,7 +20,18 @@ class Santa
 end
 
 
-santasteve = Santa.new
+# santasteve = Santa.new
+# santasteve.speak
+# santasteve.eat_milk_and_cookies("chocolate chip")
 
-santasteve.speak
-santasteve.eat_milk_and_cookies("chocolate chip")
+santas = []
+
+genders=["male","female","agender","gender fluid"]
+ethnicities=["black", "white", "Colombian", "Iranian-American", "Sioux", "Japanese", "Polish", "South African", "British", "Indian"]
+
+
+ethnicities.length.times do |i|
+	santas << Santa.new(genders[i % genders.length],ethnicities[i])
+end
+
+p santas
