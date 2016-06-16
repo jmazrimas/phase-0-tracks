2338aaf562a53e58bc1a27b0736760a66fbd87c4
jmazrimas <joe.mazrimas@gmail.com>
2016-06-16 11:51:31 -0500

    add working count array to longest_finder

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,22 @@
 
+//PSEUDO CODE FOR LONGEST STRING FINDER
+// Function will take array as an input
+// Loop over the array and create a second array with the lengths of each string
+// Non-destructively sort the count array so that the biggest number is at the top
+// 	numberarray[0] is now the length of the string we want
+// Get the index of that number in the number array and we know that the same index in the word array is the corresponding word
+// Return that word 
 
 var longest_finder = function(string_array) {
-
+	var counts = []
+	for (i=0; i<string_array.length; i++) {
+		counts.push(string_array[i].length)
+	}
+	console.log(counts)
 }
 
-var words = ["some words", "other stuff"]
+var string_array = ["string 1", "string 2","the longest one"];
 
-console.log(words.length)
+longest_finder(string_array);
+
+//console.log(string_array.length);
